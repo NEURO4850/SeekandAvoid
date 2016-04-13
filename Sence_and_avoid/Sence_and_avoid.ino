a650094d22134b0767e71fc2520552b193e23770
codarth <zimquo@gmail.com>
2016-04-12 22:31:06 -0600

    Works, imlement to their code

--- Sence_and_avoid/Sence_and_avoid.ino
+++ Sence_and_avoid/Sence_and_avoid.ino
@@ -8,27 +8,29 @@ Adafruit_StepperMotor *myMotor = AFMS.getStepper(360, 2);
 const int pingPin = 7;
 
 double angle, newAngle;
-double x=-2;
-double y=2;
+double x = 0;
+double y = -1;
 double angleDiff;
-double counter=0;
+double counter = 0;
+double backAngle;
 
 void setup() {
   AFMS.begin();
   Serial.begin(9600);
   myMotor->setSpeed(80);
+
 }
 
 void loop() {
 
   //get pack from one group, if no obstructions forward to wheels,
   //else alter pack to avoid obstacle
-  y*=-1;
-  Serial.println(x);
+  //y*=-1;
+  //Serial.println(x);
   //Coord to angle
   newAngle = coorToAng(x, y);
   angleDiff = newAngle - counter;
-  Serial.println(newAngle);
+  //Serial.println(newAngle);
   //get direction from other group
   //newAngle=40;
   // If result is larger then 360 then do nothing, or something they tell us not to move
@@ -40,7 +42,7 @@ void loop() {
     go(angleDiff, 0);
   } else {
     idle();
-    Serial.println("idleing!");
+    //Serial.println("idleing!");
     // If angle changed then check for obstacle in new direction
   }
 
@@ -50,7 +52,7 @@ void loop() {
 }
 
 // Scan the direction facing, return true if there is a obstacle
-bool checkDir() {
+long getDist() {
   long duration;
 
   pinMode(pingPin, OUTPUT);
@@ -63,20 +65,51 @@ bool checkDir() {
   duration = pulseIn(pingPin, HIGH);
   long cm = duration / 29 / 2;
 
+  return cm;
+}
+
+void checkDir() {
+  long cm = getDist();
   if (cm < 50) {
-    return true;
+    x = 0;
+    y = 0;
+    Serial.println(cm);
+    Serial.println("something in the way");
+    myMotor->step(50, 0, DOUBLE);
+    Serial.println("Clock 90");
+    long sideCM = getDist();
+    if (sideCM < 50) {
+      Serial.println("something to the right");
+      myMotor->step(100, 1, DOUBLE);
+      Serial.println("Counter 180");
+      sideCM = getDist();
+      myMotor->step(50, 0, DOUBLE);
+      Serial.println("Clock 90");
+      if (sideCM < 50) {
+        x *= -1;
+        y *= -1;
+        Serial.println("reverse");
+      } else {
+        angToCoor(90);
+        Serial.println("left");
+      }
+    } else {
+      angToCoor(-90);
+      myMotor->step(50, 0, DOUBLE);
+      Serial.println("Right");
+    }
   }
-  return false;
 }
 
 
-void go(int angle, int direct) {
-  angle=abs(angle);
-  int steps=angle/1.8;
+void go(double angle, int direct) {
+  angle = abs(angle);
+  int steps = angle / 1.8;
   myMotor->step(steps, direct, DOUBLE);
 
+  checkDir();
   delay(100);
-  
+
 }
 
 void idle() {
@@ -87,14 +120,19 @@ void idle() {
 double coorToAng(double x, double y) {
   double ang = atan2(y, x);
   ang = ang * (180 / 3.14159265359);
- /* if (x < 0 && y > 0) {
-    ang += 180;
-  } else if (x < 0 && y < 0) {
-    ang -= 180;
-  } else {
-    ang = ang;
-  }
+  /* if (x < 0 && y > 0) {
+     ang += 180;
+    } else if (x < 0 && y < 0) {
+     ang -= 180;
+    } else {
+     ang = ang;
+    }
   */
   return ang;
+}
+
+//get Coord from angle
+void angToCoor(int w) {
+  y = tan(newAngle + w) * x;
 }
 
